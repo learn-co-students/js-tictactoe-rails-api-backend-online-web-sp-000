528c1034f50ac84937608297972b39bcf293011f
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-25 15:48:02 -0500

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,22 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games, status: 201
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: 201
+  end
+
+  def create
+    @game = Game.create(state: params[:state])
+    render json: @game, status: 201
+  end
   
-    def index
-    end
-  
-    def show
-      @game = game.find(params[:id])
-    end
-  
-    def create
-      @game = Game.create(state: params[:state])
-      render json: @game, status: 201
-    end
-  
-    def update
-      @game = game.find(params[:id])
-    end
+  def update
+    @game = Game.find(params[:id])
+    @game.update(state: params[:state])
+    render json: @game, status: 201
+  end
 end
