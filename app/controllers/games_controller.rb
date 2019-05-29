00bb015986d94f2b7914785971a3a551bc234b45
anupkhadka <akhadka.dev@gmail.com>
2019-05-29 14:15:42 -0400

    Add ability to create a game from params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,13 +2,15 @@ class GamesController < ApplicationController
   # Add your GamesController code here
 
   def create
+    @game = Game.create(state: params[:state])
+    render json: @game, status:201
   end
 
   def show
   end
 
   def update
-    
+
   end
 
   def index
