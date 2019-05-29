e7641a89432de9ff4608b3e70e603b81a434e122
anupkhadka <akhadka.dev@gmail.com>
2019-05-29 14:19:57 -0400

    Add code to display a game. Add code to adhere to v1.0 of the JSON:API specification when serializing objects. AMS v0.10.6 defaults to its own serialization strategy, so we need to do a bit of manual configuration. Create a file called active_model_serializer.rb inside config/initializers and add code to tell AMS to use the :json_api serialization adapter, to include a top-level object, and to adhere to v1.0 of the JSON:API specification.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  before_action :set_game, only: [:show, :update]
 
   def create
     @game = Game.create(state: params[:state])
@@ -7,6 +7,7 @@ class GamesController < ApplicationController
   end
 
   def show
+    render json: @game, status: 201
   end
 
   def update
@@ -15,6 +16,12 @@ class GamesController < ApplicationController
 
   def index
 
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 
 end
