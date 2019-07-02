0597e7ff0b37bc0ea2f5dc208dcf49c7fe1ef925
Sharon Alajajian <sharonoptsout@gmail.com>
2019-07-02 16:37:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  #Create — POST — /games
+  #Show — GET — /games/:id
+  #Update — PATCH — /games/:id
+  #Index — GET — /games
+  resources :games
 end
